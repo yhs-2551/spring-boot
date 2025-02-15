5e8c9a9102865de2bbb58c912b646d39fb41d9a9
yhs <yhsdeveloper8746@gmail.com>
2025-02-15 20:20:07 +0900

    Ci-cd.yml 수정 - 배포 부분

--- blog-springboot-jpa/src/main/java/com/yhs/blog/springboot/jpa/domain/auth/token/filter/TokenAuthenticationFilter.java
+++ blog-springboot-jpa/src/main/java/com/yhs/blog/springboot/jpa/domain/auth/token/filter/TokenAuthenticationFilter.java
@@ -34,7 +34,7 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
                         FilterChain filterChain)
                         throws ServletException, IOException {
 
-                log.info("[TokenAuthenticationFilter] doFilterInternal 메sㄴㄹㅇㄴㄹㄴfsdfdsfsssfdsfsㄹㄴㄹsfsㄹㄴㄹㄴㄹㄴ시sfsfsfs람쥐dfsffssdfsfsffssㅇㅇㅇㅇㄴㄹㄴㄹㅇㅇ.");
+                log.info("[TokenAuthenticationFilter] doFilterInternal 123456.");
 
                 String method = request.getMethod();
                 String requestURI = request.getRequestURI();
