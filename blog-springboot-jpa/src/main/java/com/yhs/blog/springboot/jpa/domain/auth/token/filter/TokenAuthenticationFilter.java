1a06c124068ec60725552017866cf6cc6bddda1b
yhs <yhsdeveloper8746@gmail.com>
2025-02-15 17:21:14 +0900

    CD 테스트-2

--- blog-springboot-jpa/src/main/java/com/yhs/blog/springboot/jpa/domain/auth/token/filter/TokenAuthenticationFilter.java
+++ blog-springboot-jpa/src/main/java/com/yhs/blog/springboot/jpa/domain/auth/token/filter/TokenAuthenticationFilter.java
@@ -34,7 +34,7 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
                         FilterChain filterChain)
                         throws ServletException, IOException {
 
-                log.info("[TokenAuthenticationFilter] doFilterInternal 메서드 시작 수정 했습니다.");
+                log.info("[TokenAuthenticationFilter] doFilterInternal 메서드 시작 수정 했습니다람쥐.");
 
                 String method = request.getMethod();
                 String requestURI = request.getRequestURI();
