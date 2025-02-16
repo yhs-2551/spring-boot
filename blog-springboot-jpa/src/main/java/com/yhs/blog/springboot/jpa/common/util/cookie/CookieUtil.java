6db95459953538662abc3c8c505dfdd253663584
yhs <yhsdeveloper8746@gmail.com>
2025-02-16 13:47:35 +0900

    fix: 리다이렉트 후 쿠키가 안보여서 재설정

--- blog-springboot-jpa/src/main/java/com/yhs/blog/springboot/jpa/common/util/cookie/CookieUtil.java
+++ blog-springboot-jpa/src/main/java/com/yhs/blog/springboot/jpa/common/util/cookie/CookieUtil.java
@@ -25,7 +25,15 @@ public class CookieUtil {
         if (ApplicationContextProvider.isProd()) {
             cookie.setSecure(true); // 쿠키가 HTTPS 연결을 통해서만 전송되도록 함.
             cookie.setAttribute("SameSite", "None"); // 크로스 사이트 요청 위조(CSRF) 공격을 방지하기 위한 쿠키 보안
-            // cookie.setDomain("blog-app-kohl-one.vercel.app"); 
+            cookie.setDomain("dduha.duckdns.org"); 
+
+            log.info("쿠키 설정 정보: name={}, path={}, domain={}, secure={}, samesite={}", 
+            name, 
+            cookie.getPath(),
+            cookie.getDomain(),
+            cookie.getSecure(),
+            cookie.getAttribute("SameSite")
+        );
         }
         // 응답에 쿠키 추가
         httpServletResponse.addCookie(cookie);
@@ -56,7 +64,15 @@ public class CookieUtil {
                 if (ApplicationContextProvider.isProd()) {
                     cookie.setSecure(true);
                     cookie.setAttribute("SameSite", "None");
-                    // cookie.setDomain("blog-app-kohl-one.vercel.app");
+                    cookie.setDomain("dduha.duckdns.org");
+
+                    log.info("쿠키 설정 정보: name={}, path={}, domain={}, secure={}, samesite={}", 
+                    name, 
+                    cookie.getPath(),
+                    cookie.getDomain(),
+                    cookie.getSecure(),
+                    cookie.getAttribute("SameSite")
+                );
                 }
 
                 response.addCookie(cookie);
